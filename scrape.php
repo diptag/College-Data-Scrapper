0b55a542536425e27649a0a03693634b17a71f56
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-14 15:47:46 +0000

    scrape.php can now scrape clg names and location

--- scrape.php
+++ scrape.php
@@ -1,13 +1,13 @@
 <?php
     //check if url is not empty
-    if (!isset($_POST["url"]))
+    /*if (!isset($_POST["url"]))
     {
         exit();    
     }
-
+    */
     //get the raw html from shiksha.com using cURL
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $_POST["url"]);
+    curl_setopt($ch, CURLOPT_URL, "http://www.shiksha.com/b-tech/colleges/b-tech-colleges-bangalore");
     curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $ret = curl_exec($ch);
@@ -19,5 +19,18 @@
         exit();
     }
 
-    // scrape data from the raw html
+    /*
+     *  scrape data from the raw html
+     */
+    // scrape college names and location
+    preg_match_all("/<h2 class=\"tuple-clg-heading\"><a.*_blank\">(.*)<\/a>\n<p>\| (.*)<\/p><\/h2>/", $ret, $matches);
+    $clg = $matches[1];
+    $location = $matches[2];
+
+    $n = 0;
+    foreach ($clg as $col)
+    {
+        print($col.", ".$location[$n]."\n");
+        $n += 1;
+    }
 ?>
