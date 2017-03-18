34bbe9475ae524ba42c9037bdc6114265f4b6b7d
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-18 11:41:17 +0000

    Modified scripts.js

--- scripts.js
+++ scripts.js
@@ -1,35 +1,63 @@
 /*global $*/
+
+// execute the code if the page is fully loaded
 $(function () {
+  
+  // execute the following code when form is submitted
   $("#form").submit(function () {
-    $("#error").css("visibility", "hidden");
-    $("#warning").css("display", "none");
+
+    // validate url entered by the user
     var pgurl = $("#urltext").val();
-    var result = (/(http[^\d]*[^\d\-])(?=-\d.*)?/).exec(pgurl);
-    if (result === null) {
+    var result = (/(http:\/\/www\.shiksha\.com\/b-tech\/colleges\/b-tech-colleges-[^\d]*[^\d\-])(?=-\d.*)?/).exec(pgurl);
+    
+    // display error if user entered invalid shiksha.com url
+    if (result === null) 
+    {
       $("#warning").css("display", "block");
       return false;
-    } else {
+    } 
+    else 
+    {
+      // display loading gif
       $("#progress").css("visibility", "visible");
+      
+      // declare status, lastpage and nextpage variables
       var lastpage = false;
       var loadstatus = true;
-      $.ajax({
-        url: "scrape.php",
-        data: {
-          pageurl: result[1]
-        },
-        async: false,
-        success: function (data) {
-          if (data.status === false)
-          {
-            $("#msg").text("Not Done");
+      var nextpage = result[1];
+      
+      // use while loop to send multiple ajax requests to scrape.php till last page is not reached 
+      while ((lastpage === false) && (loadstatus === true))
+      {
+        $.ajax({
+          url: "scrape.php",
+          data: {
+            pageurl: nextpage
+          },
+          async: false,
+          success: function (data) {
+            if (data.status === false)
+            {
+              // upadate loadstatus
+              loadstatus = false;
+            }
+            else
+            {
+              // upadate lastpage and nextpage
+              if (data.lastpage === false)
+              {
+                lastpage = false;
+                nextpage = data.nextpage;
+              }
+              else
+              {
+                lastpage = true;
+              }
+            }
           }
-          else
-          {
-            
-          }
-        }
-      });
-      setTimeout(function () { alert("done");}, 3000);
-  }
+        });
+      }
+      return true;
+    }
   });
 });
