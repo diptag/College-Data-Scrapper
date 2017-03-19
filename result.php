d486405521f8a0348a02c78ff33473473adbca5d
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-19 16:17:55 +0000

    Added result.php

---
+++ result.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Engineering College Finder</title>
+</head>
+
+<body>
+<div id="wrapper">
+  <header>
+        	<h1>ENGINEERING COLLEGE FINDER</h1>
+  </header>
+      <div id="main">
+            <table>
+            	<tr>
+                    <th>S. No.</th>
+                    <th>College/Institute</th>
+                    <th>Location</th>
+                    <th>Facilities</th>
+                    <th>Reviews</th>
+            	</tr>
+            </table>
+            <p id="back"><a href="index.html">Go Back</a></p>
+        </div>
+        <footer>
+        	<span id="footer-text">Created by Diptanshu Agarwal &copy;</span>
+        </footer>
+</div>
+</body>
+</html>
