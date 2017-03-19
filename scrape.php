eb6299281ddf1e1d66e707a8199f1d2e5fec64f8
Diptanshu Agarwal <diptanshu@iitg.ernet.in>
2017-03-19 13:54:07 +0000

    Modified scrape.php for debugging

--- scrape.php
+++ scrape.php
@@ -1,14 +1,13 @@
 <?php
-     // Send content type header
+    // Send content type header
     header ("Content-type: application/json");
     
     //check if url is not empty, else return not loaded
-    if (!isset($_GET["pageurl"]))
+    if (!isset($_GET["pageurl"]) && !isset($_GET["scrapeid"]))
     {
         print(json_encode(array("status" => false), JSON_PRETTY_PRINT));
         exit();    
     }
-    
     
     //get the raw html from shiksha.com using cURL
     $ch = curl_init();
@@ -37,7 +36,7 @@
     {
         // scrape college names and location
         preg_match("/<h2 class=\"tuple-clg-heading\"><a.*_blank\">(.*)<\/a>\n<p>\| (.*)<\/p><\/h2>/", $match, $matches_1);
-        $clg[$m] = $matches_1[1];
+        $colleges[$m] = $matches_1[1];
         $location[$m] = $matches_1[2];
     
         // scrape part of html containing facilities for the college
@@ -57,10 +56,71 @@
         $m += 1;
     }
     
+    /*
+    * save scraped data in database
+    */
+    
+    // connect to database
+    $dbh = new PDO("mysql:host=127.0.0.1;dbname=project_1", "dadiptanshu", "v1kCjsvLYytrBTGV");
+    
+    // get scrape id from databse if scrape id given through GET request is 0 for currnet scrape
+    if ($_GET["scrapeid"] === 0)
+    {
+        $lastscrape = $dbh->query("SELECT MAX(scrape_id) AS lastscrape FROM colleges");
+        $temp = $lastscrape->fetch(PDO::FETCH_ASSOC);
+        if ($temp["lastscrape"] === NULL)
+            $nextscrape = 1;
+        else
+            $nextscrape = $temp["lastscrape"] + 1;
+    }
+    else 
+    {
+        $nextscrape = $_GET["scrapeid"];
+    }
+    
+    // prepare insert statement for inserting data into table college
+    $stmt = $dbh->prepare("INSERT INTO colleges (scrape_id, college, location, reviews) VALUES (".$nextscrape.", :college, :location, :reviews)");
+    
+    // insert college name, location and reviews of each collegein database
+    $m = 0;
+    foreach ($colleges as $clg)
+    {
+        $stmt->bindParam(":college", $clg);
+        $stmt->bindParam(":location", $location[$m]);
+        $stmt->bindParam(":reviews", $reviews[$m]);
+        $stmt->execute();
+        $m++;
+    }
+    
+    // get entry id of first college enetered into the database
+    $entry = $dbh->query("SELECT MAX(entry_id) AS entry FROM colleges WHERE college = '".$colleges[0]."'");
+    $tmp = $entry->fetch(PDO::FETCH_ASSOC);
+    $nextid = $tmp["entry"];
+    
+    // prepare insert statement for inserting data into table facilities
+    $stmt = $dbh->prepare("INSERT INTO facilities (entry_id, facility) VALUES (:id, :facility)");
+    
+    // insert facilities into table facilities
+    $m = 0;
+    foreach ($facilities as $facility)
+    {
+        foreach ($facility as $fty)
+        {
+            $id = $nextid + $m;
+            $stmt->bindParam(":id", $id);
+            $stmt->bindParam(":facility", $fty);
+            $stmt->execute();
+        }
+        $m++;
+    }
+    
+    // close connection
+    $dbh = null;
+    
     // Check if it is the last page and send json data
     if (preg_match("/next linkpagination\"><a data-page=\"\d\" href = (.*)><i/", $ret, $next))
-        print(json_encode(array("status" => true, "lastpage" => false, "nextpage" => $next[1]), JSON_PRETTY_PRINT));
+        print(json_encode(array("status" => true, "lastpage" => false, "nextpage" => $next[1], "scrapeid" => $nextscrape), JSON_PRETTY_PRINT));
     else
         print(json_encode(array("status" => true, "lastpage" => true), JSON_PRETTY_PRINT));
-
+        exit();
 ?>
